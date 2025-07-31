88795b068bb9b3d1b82dd4f739d1247c0b33ce80
leonardo_Wer <name89213126414@gmail.com>
2025-07-31 22:07:47 +0300

    Оптимизировал анимацию секции обо мне

--- src/components/sections/AboutSection/AboutSection.js
+++ src/components/sections/AboutSection/AboutSection.js
@@ -64,42 +64,30 @@ export function createAboutSection() {
 
 function initGsapAnimations(aboutContainer, aboutTextContainer, aboutTiles) {
 
-    const animationDuration = 2000; // Продолжительность анимации
-    const tileDelay = 0.2; // Промежуток между появлением плиток
-
-    // 1. Закрепление текста
-    gsap.to(aboutTextContainer, {
-        scrollTrigger: {
-            trigger: aboutContainer,
-            start: "top top",
-            end: `+=${animationDuration}`,
-            pin: true,
-            pinSpacing: true,
-            scrub: true,
-        }
-    });
+    const animationDuration = 1500; // Продолжительность анимации
+    const tileDelay = 0.1; // Промежуток между появлением плиток
 
     // 2. Анимация плиток (с использованием Timeline)
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: aboutContainer,
-            start: "top top",
-            end: `+=${animationDuration}`,
-            scrub: true,
-            pin: false,
-            invalidateOnRefresh: true, //  Пересчет при изменении размеров
-        }
-    });
-
+    const tl = gsap.timeline();
     aboutTiles.forEach((tile, index) => {
         tl.fromTo(tile, {
             yPercent: 100 //  Начальное положение (вне экрана снизу)
         }, {
-            yPercent: -200, //  Сверху экрана
+            yPercent: -180, //  Сверху экрана
             autoAlpha: 1,   //  Появление плитки
             ease: "power2.out",
-            duration: 1, //  Длительность анимации
             delay: index * tileDelay, // Задержка для появления по очереди
         }, 0); //  0 означает, что все анимации будут выполняться последовательно
     });
+
+    ScrollTrigger.create({
+        trigger: aboutContainer,
+        start: "top top",
+        end: `+=${animationDuration}`,
+        pin: aboutContainer,
+        pinSpacing: true,
+        scrub: true,
+        animation: tl,
+    })
+
 }
