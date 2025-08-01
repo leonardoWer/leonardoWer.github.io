2d41e6f0d51943c3aa8b23481c7bb2f69b777ef6
leonardo_Wer <name89213126414@gmail.com>
2025-08-01 23:10:39 +0300

    Мои работы плитки

--- src/components/sections/MyWorksSection/MyWorksSection.js
+++ src/components/sections/MyWorksSection/MyWorksSection.js
@@ -1,4 +1,6 @@
 import styles from './MyWorksSection.module.css'
+import {createWorkTile} from "s/components/WorkTile/WorkTile.js";
+
 import gsap from 'gsap'
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
@@ -19,51 +21,67 @@ export function createMyWorksSection() {
           </div>
         </div>
 
-        <div class="container ${styles.myWorks__bottom}">
-          <div class="${styles.myWorksBottom__item}"></div>
+        <div class="${styles.myWorks__bottom}">
+          <div class="${styles.myWorksBottom__textContainer}">
+            <span class="${styles.myWorksBottomTextContainer__title}">Каждый проект</span>
+            <span class="${styles.myWorksBottomTextContainer__uniq}">уникален</span>
+          </div>
+          <div class="${styles.myWorksBottom__workTilesContainer}"></div>
         </div>
     `
 
-    // Элементы
-    const topContainer= section.querySelector(`.${styles.myWorks__top}`)
-    const topContentContainer = section.querySelector(`.${styles.myWorksTop__bg}`)
-    const titleText = section.querySelector(`.${styles.myWorksTitle}`)
-    const worksTopPreviewContainer = section.querySelector(`.${styles.myWorksTop__previewContainer}`)
+    // Элементы верхней части
+    const topContainer= section.querySelector(`.${styles.myWorks__top}`);
+    const topContentContainer = section.querySelector(`.${styles.myWorksTop__bg}`);
+    const titleText = section.querySelector(`.${styles.myWorksTitle}`);
+    const worksTopPreviewContainer = section.querySelector(`.${styles.myWorksTop__previewContainer}`);
+
+    // Элементы нижней части
+    const bottomContainer = section.querySelector(`.${styles.myWorks__bottom}`);
+    const workTilesContainer = section.querySelector(`.${styles.myWorksBottom__workTilesContainer}`);
+    const bottomTextContainer = section.querySelector(`.${styles.myWorksBottom__textContainer}`);
 
     // Мои работы
     const myWorksData = [
         {
             name: "Spider-Man-Movies",
-            titlePhoto: "spider-man-movies.png",
-            stack: ["vite", "gsap"]
+            titleImg: "spider-man-movies.png",
+            stack: ["vite", "gsap"],
+            link: "leonardoWer.github.io/Spider-Man-Movie",
         },
         {
             name: "basketball-courts",
-            titlePhoto: "basketball-courts.png",
-            stack: ["vite", "gsap"]
+            titleImg: "basketball-courts.png",
+            stack: ["vite", "gsap", "ymaps API"],
+            link: "leonardoWer.github.io/basketball-courts",
         },
         {
-            name: "Spider-Man-Movies",
-            titlePhoto: "spider-man-movies.png",
-            stack: ["vite", "gsap"]
-        },
-        {
-            name: "basketball-courts",
-            titlePhoto: "basketball-courts.png",
-            stack: ["vite", "gsap"]
+            name: "Петербург Бенуа",
+            titleImg: "benua-hsitmo.png",
+            stack: ["vanilla", "ymaps API"],
+            link: "http://benua.hsitmo.ru",
         },
     ];
 
     const myWorksPreviewTilesData = []
 
     myWorksData.forEach(work => {
-        const previewTile = createWorkPreviewTile({img: work.titlePhoto})
+        // Верхняя часть
+        const previewTile = createWorkPreviewTile({img: work.titleImg})
         myWorksPreviewTilesData.unshift(previewTile);
+        worksTopPreviewContainer.appendChild(previewTile);
 
-        worksTopPreviewContainer.appendChild(previewTile)
+        // Нижняя часть
+        const workTile = createWorkTile({
+            title: work.name,
+            titleImg: photoPath + work.titleImg,
+            stack: work.stack,
+            link: work.link});
+        workTilesContainer.appendChild(workTile);
     })
 
-    initGsapAnimations(topContainer, topContentContainer, titleText, worksTopPreviewContainer, myWorksPreviewTilesData);
+    initWorksTopGsapAnimations(topContainer, topContentContainer, titleText, worksTopPreviewContainer, myWorksPreviewTilesData);
+    initWorksBottomGsapAnimations(bottomContainer, workTilesContainer, bottomTextContainer);
 
     return section;
 }
@@ -78,7 +96,7 @@ function createWorkPreviewTile({img}) {
     return tile;
 }
 
-function initGsapAnimations(topContainer, topBg, titleText, myWorksTopPreviewContainer, myWorksPreviewTilesData) {
+function initWorksTopGsapAnimations(topContainer, topBg, titleText, myWorksTopPreviewContainer, myWorksPreviewTilesData) {
 
     // 1. Анимация фона (управляемая ScrollTrigger)
     gsap.fromTo(topBg, {
@@ -156,4 +174,27 @@ function initGsapAnimations(topContainer, topBg, titleText, myWorksTopPreviewCon
         animation: gsap.timeline().add(fadeInTl).add(previewContainerFadeInTl).add(tilesRotateAndSpacingTl).add(tilesFadeOutTl),
     });
 
+}
+
+function initWorksBottomGsapAnimations(bottomContainer, workTilesContainer, textContainer) {
+    const tl = gsap.timeline();
+    tl.to(workTilesContainer, {
+        xPercent: -70,
+        ease: "power1.inOut",
+        duration: 2
+    }, 0)
+        .to(textContainer, {
+            opacity: 0,
+            ease: "power3.in",
+            duration: 0.4
+        }, 0)
+
+    ScrollTrigger.create({
+        trigger: bottomContainer,
+        start: "top top",
+        end: "+=2000",
+        pin: bottomContainer,
+        scrub: true,
+        animation: gsap.timeline().add(tl),
+    });
 }
