ab59ac7e1097ecb12df0717d877e4c0a44a1d600
leonardo_Wer <name89213126414@gmail.com>
2025-08-02 19:42:11 +0300

    Анимация переворота текста

--- src/components/Links/EllipsLink/EllipsLink.js
+++ src/components/Links/EllipsLink/EllipsLink.js
@@ -1,10 +1,13 @@
 import styles from "./EllipsLink.module.css"
 import {createLink} from "s/components/Links/link.js";
 
+import gsap from "gsap";
+
 export function createEllipsLink({title, onClick, style}) {
-    const link = createLink({title, onClick});
+    const link = createLink({title: "", onClick: onClick});
     link.classList = styles.ellipsLink;
 
+    // Цвет
     switch (style) {
         case "dark":
             link.classList.add(styles.ellipsLink__dark);
@@ -14,5 +17,43 @@ export function createEllipsLink({title, onClick, style}) {
             break;
     }
 
+    // Текст
+    link.innerHTML = `
+        <div class=${styles.ellipsLink__textContainer}>
+            <span class="${styles.originalText}">${title}</span>
+            <span class="${styles.newText}">${title}</span>
+        </div>
+    `;
+
+    const origTextEl = link.querySelector(`.${styles.originalText}`);
+    const newTextEl = link.querySelector(`.${styles.newText}`);
+    initHoverAnimations(link, origTextEl, newTextEl);
+
     return link;
+}
+
+function initHoverAnimations(link, origTextEl, newTextEl) {
+
+    const duration = 0.5
+    const tl = gsap.timeline({paused: true});
+    tl.to(origTextEl, {
+        duration: duration,
+        yPercent: -100,
+        rotationX: -90,
+        ease: "power2.inOut",
+    }, 0)
+        .to(newTextEl, {
+            duration: duration,
+            yPercent: -100,
+            rotationY: 0,
+            ease: "power2.inOut",
+        }, 0);
+
+    link.addEventListener("mouseenter", () => {
+        tl.play()
+    });
+
+    link.addEventListener("mouseleave", () => {
+        tl.reverse();
+    });
 }
